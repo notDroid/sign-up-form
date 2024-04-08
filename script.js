357a80dbced535d7d0a9e3bc00cefd2641a4a47c
Wali Mirza <walimirza9982@gmail.com>
2024-04-08 02:47:48 -0400

    Finished project, added form functionality, finalized css style changes and html page design.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi')
+const pswd = document.querySelector('#pswd');
+const cp = document.querySelector('#cp');
+
+cp.addEventListener('input', validate)
+
+function validate() {
+    if (pswd.value != cp.value) {
+        cp.setCustomValidity('Passwords do not match')
+    } else {
+        cp.setCustomValidity('')
+    }
+}
